b881103bfb4d296492e059dcffedc285839304ad
Vedant Baretha <vedantbaretha7@gmail.com>
2026-07-10 06:26:13 +0530

    Create IncrementDecrementOperator2Ex2.java

---
+++ IncrementDecrementOperator2Ex2.java
@@ -0,0 +1,22 @@
+public class IncrementDecrementOperatorEx2 {
+
+	public static void main(String[] args) {
+		int a=10;
+		a++;//postfix
+		System.out.println(a);
+		int b=10;
+		++b;//prefix
+		System.out.println(b);
+		int c=10;
+		c--;
+		System.out.println(c);
+		int d=10;
+		--d;
+		System.out.println(d);
+	}
+
+}
+
+//Pre - First update then use
+//Post- First use then Update
+
